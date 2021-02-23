0ee571c663b8c23ece0ac2b73a8064ec23b1f7ea
Scottie <mr.horne@gmail.com>
2021-02-23 11:23:40 -0600

    corrected the UserController and the users template.

--- src/main/java/com/tts/reviewspring/controller/UserController.java
+++ src/main/java/com/tts/reviewspring/controller/UserController.java
@@ -5,12 +5,15 @@ import com.tts.reviewspring.model.User;
 import com.tts.reviewspring.service.TweetService;
 import com.tts.reviewspring.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.HashMap;
 import java.util.List;
 
+@Controller
 public class UserController {
 
     @Autowired
@@ -31,7 +34,24 @@ public class UserController {
         model.addAttribute("tweetList", tweets);
 
         return "user";
+    }
 
+    @GetMapping(value = "/users")
+    public String getUsers(Model model){
+
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        setTweetCounts(users, model);
+        return "users";
+    }
+
+    private void setTweetCounts(List<User> users, Model model){
+        HashMap<String, Integer> tweetCounts = new HashMap<>();
+        for (User user : users){
+            List<Tweet> tweets = tweetService.findAllByUser(user);
+            tweetCounts.put(user.getUsername(), tweets.size());
+        }
+        model.addAttribute("tweetCounts", tweetCounts);
     }
 
 }
